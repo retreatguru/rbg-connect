eab776f80860c95f5957b45f4630a912905fead7
Ryan Dury <ryandury@gmail.com>
2015-06-11 12:04:05 -0700

    Clean up files, prevent style conflicts

--- programs-remote-listings/templates/shortcode-programs-table.php
+++ programs-remote-listings/templates/shortcode-programs-table.php
@@ -5,30 +5,26 @@ if(is_array($shortcode_atts)) extract($shortcode_atts);
 <table class="pure-table rs-program rs-group shortcode table">
     <thead>
     <tr>
-        <?php if(isset($show_date)){ ?><th>Dates</th><?php } ?>
-        <?php if(isset($show_availability)){ ?><th style="text-align: center;">Available Spots</th><?php } ?>
-        <?php if(isset($show_title)){ ?><th>Name</th><?php } ?>
-        <?php if(isset($show_teachers)){ ?><th>Hosts</th><?php } ?>
-        <?php if(isset($show_more_link)){ ?><th style="text-align: center;">Details</th><?php } ?>
-        <?php if(isset($show_register_link)){ ?><th style="text-align: center;">Register</th><?php } ?>
+        <?php if(isset($show_date)){ ?><th class="rs-dates">Dates</th><?php } ?>
+        <?php if(isset($show_availability)){ ?><th class="rs-availability">Available Spots</th><?php } ?>
+        <?php if(isset($show_title)){ ?><th class="rs-title">Name</th><?php } ?>
+        <?php if(isset($show_teachers)){ ?><th class="rs-teachers">Hosts</th><?php } ?>
+        <?php if(isset($show_more_link)){ ?><th class="rs-show-more-link" >Details</th><?php } ?>
+        <?php if(isset($show_register_link)){ ?><th class="rs-show-register-link">Register</th><?php } ?>
     </tr>
     </thead>
-
     <tbody>
-
     <?php foreach($rs_the_programs as $program): ?>
-
         <tr>
-
             <?php if(isset($show_date)){ ?>
-            <td>
+            <td class="rs-dates">
                 <?php if ( isset($program->date) ) : ?>
                     <?php echo $program->date; ?>
                 <?php endif; ?>
                 <?php } ?>
 
                 <?php if(isset($show_availability)) : ?>
-            <td class="availability" style="text-align: center;">
+            <td class="rs-availability">
                 <?php if ( isset($program->date) ) : ?>
                     <?php echo $program->registration_spaces_available; ?>
                 <?php endif; ?>
@@ -36,7 +32,7 @@ if(is_array($shortcode_atts)) extract($shortcode_atts);
         <?php endif; ?>
 
             <?php if(isset($show_title)) : ?>
-                <td class="title">
+                <td class="rs-title">
                     <?php if ( isset($program->title) ) : ?>
                         <?php echo $program->title; ?>
                     <?php endif; ?>
@@ -44,7 +40,7 @@ if(is_array($shortcode_atts)) extract($shortcode_atts);
             <?php endif; ?>
 
             <?php if(isset($show_teachers)) : ?>
-                <td class="teachers" style="text-align: left;">
+                <td class="rs-teachers">
                     <?php if ( isset($program->teacher_details->teacher_objects) ) : ?>
                         <?php echo $program->teacher_details->teacher_list; ?>
                     <?php endif; ?>
@@ -52,13 +48,13 @@ if(is_array($shortcode_atts)) extract($shortcode_atts);
             <?php endif; ?>
 
             <?php if(isset($show_more_link)) : ?>
-                <td class="show-more-link" style="text-align: center;">
+                <td class="rs-show-more-link">
                     <?php echo '<a href="/'.$RS_Connect->style.'/'.$program->ID.'/'.$program->slug.'">View Details</a>'; ?>
                 </td>
             <?php endif; ?>
 
             <?php if(isset($show_register_link)) : ?>
-                <td class="status" style="text-align: center;">
+                <td class="rs-show-register-link">
                     <?php if ($program->registration_bookable): ?>
                         <a href="<?php echo $program->registration_link; ?>" target="_blank">Register Now</a>
                     <?php else: ?>
@@ -66,10 +62,7 @@ if(is_array($shortcode_atts)) extract($shortcode_atts);
                     <?php endif; ?>
                 </td>
             <?php endif; ?>
-
         </tr>
-
     <?php endforeach; ?>
-
     </tbody>
 </table>
