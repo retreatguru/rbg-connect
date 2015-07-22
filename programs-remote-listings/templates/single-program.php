a21249349ca0777d67a8d5fe262ee40cd6dace23
Ryan Dury <ryandury@gmail.com>
2015-07-22 15:24:34 -0700

    Visual improvements to single-program

--- programs-remote-listings/templates/single-program.php
+++ programs-remote-listings/templates/single-program.php
@@ -10,47 +10,56 @@ if(isset($options['rs_template']['before'])) echo $options['rs_template']['befor
 <?php global $rs_the_program; ?>
 
         <article class="page type-page status-publish single-program">
+
+            <div class="entry-content">
+
+                <div class="rs-program-meta">
+
+                    <?php  if ( $rs_the_program->photo_details ) : ?>
+                    <div class="rs-program-photo">
+                        <img src="<?php echo $rs_the_program->photo_details->large->url; ?>" width="100%">
+                    </div>
+                    <?php endif; ?>
+
+                    <div class="rs-meta-content-container">
+
+                        <?php // Pricing ?>
+                        <?php if ( isset($rs_the_program->price_details) ) : ?>
+                            <div class="rs-program-price"><?php echo $rs_the_program->price_details ?></div>
+                        <?php endif; ?>
+
+                        <?php // Datetime details ?>
+                        <?php if ( isset($rs_the_program->date_time) ) : ?>
+                            <p class="rs-program-datetime"><span class="rs-program-label">Date and Time Details:</span> <?php echo $rs_the_program->date_time; ?></p>
+                        <?php endif; ?>
+
+                        <?php // Location ?>
+                        <?php if ( isset($rs_the_program->location) ) : ?>
+                            <p class="rs-program-location"><span class="rs-program-label">Location:</span> <?php echo $rs_the_program->location; ?></p>
+                        <?php endif; ?>
+
+                        <?php // Contact details ?>
+                        <?php if ( isset($rs_the_program->contact) ) : ?>
+                            <p class="rs-program-contact"><?php echo $rs_the_program->contact ?></p>
+                        <?php endif; ?>
+
+                        <?php // Custom fields ?>
+                        <?php if ( $rs_the_program->custom ) : ?>
+                            <div class="rs-program-custom-wrap"><?php echo $rs_the_program->custom; ?></div>
+                        <?php endif; ?>
+
+                    </div>
+                </div>
+
                 <header class="entry-header">
                 <h1 class="rs-program-title"><?php echo $rs_the_program->title; ?></h1>
                 </header>
 
-                <div class="entry-content">
-                <?php  if ( $rs_the_program->photo_details ) : ?>
-                <div class="rs-program-photo">
-                    <img src="<?php echo $rs_the_program->photo_details->medium->url; ?>" width="<? echo $rs_the_program->photo_details->medium->width; ?>" height="<? echo $rs_the_program->photo_details->medium->height; ?>">
-                </div>
-                <?php endif; ?>
+
+
                 <p class="rs-program-date"><?php echo $rs_the_program->date; ?></p>
 
                 <div class="rs-regsitration-wrap"><?php echo $rs_the_program->registration_action; ?></div>
-
-                <div class="rs-program-meta">
-
-                    <?php // Pricing ?>
-                    <?php if ( isset($rs_the_program->price_details) ) : ?>
-                        <div class="rs-program-price"><?php echo $rs_the_program->price_details ?></div>
-                    <?php endif; ?>
-
-                    <?php // Datetime details ?>
-                    <?php if ( isset($rs_the_program->date_time) ) : ?>
-                        <p class="rs-program-datetime"><span class="rs-program-label">Date and Time Details:</span> <?php echo $rs_the_program->date_time; ?></p>
-                    <?php endif; ?>
-
-                    <?php // Location ?>
-                    <?php if ( isset($rs_the_program->location) ) : ?>
-                        <p class="rs-program-location"><span class="rs-program-label">Location:</span> <?php echo $rs_the_program->location; ?></p>
-                    <?php endif; ?>
-
-                    <?php // Contact details ?>
-                    <?php if ( isset($rs_the_program->contact) ) : ?>
-                        <p class="rs-program-contact"><?php echo $rs_the_program->contact ?></p>
-                    <?php endif; ?>
-
-                    <?php // Custom fields ?>
-                    <?php if ( $rs_the_program->custom ) : ?>
-                        <div class="rs-program-custom-wrap"><?php echo $rs_the_program->custom; ?></div>
-                    <?php endif; ?>
-                </div>
 
                 <?php // Program Details ?>
                 <div class="rs-program-content">
@@ -73,7 +82,7 @@ if(isset($options['rs_template']['before'])) echo $options['rs_template']['befor
             <?php endif; ?>
 
             <?php if ( $rs_the_program->teacher_details->teacher_objects ) : ?>
-                <div style="clear:left; float:left;">
+
                 <h3>Teachers</h3>
                     <?php foreach($rs_the_program->teacher_details->teacher_objects as $teacher) : ?>
                         <div class="teacher" style="clear:left; float:left; padding:10px 0;">
@@ -86,9 +95,11 @@ if(isset($options['rs_template']['before'])) echo $options['rs_template']['befor
                             </p>
                         </div>
                     <?php endforeach; ?>
-                </div>
+
             <?php endif; ?>
-        <div style="clear:both;"></div>
+</div>
+          <div style="clear:both;"></div>
+
 </article>
 
 <?php if(isset($options['rs_template']['after'])) echo $options['rs_template']['after']; ?>
