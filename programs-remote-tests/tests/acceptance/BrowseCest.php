c3d6c1febeb6889fb7266e918610d6f9c3d47dea
dwenaus <deryk@bluemandala.com>
2015-09-21 14:09:26 -0700

    change test

--- programs-remote-tests/tests/acceptance/BrowseCest.php
+++ programs-remote-tests/tests/acceptance/BrowseCest.php
@@ -6,6 +6,6 @@ class BrowseCest
     public function homePage(AcceptanceTester $I)
     {
         $I->amOnPage('/hello-world/');
-        $I->see('Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!');
+        $I->see('Welcome to WordPress');
     }
 }
