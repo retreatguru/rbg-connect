51fa038820b8b150ff17c4fc026ceb763d28651a
Ryan Dury <ryandury@gmail.com>
2015-09-21 16:08:17 -0700

    Click on program?

--- programs-remote-tests/tests/acceptance/viewProgramCest.php
+++ programs-remote-tests/tests/acceptance/viewProgramCest.php
@@ -5,8 +5,8 @@ class viewProgramCest
 {
     public function viewProgram(AcceptanceTester $I)
     {
-        $I->amOnPage('/program/4/example-program/');
-        $I->see('Example Program');
+        $I->amOnPage('/programs/');
+        $I->click('Example Program');
         $I->see('Register Now');
         $I->see('$100.00');
         //todo: Create or Modify seeder to include a program with more details
