0336a0e0a6a1b8b43a1f8278b9beae1d8d6f85f2
jammin <jammin.irving@gmail.com>
2016-02-18 10:40:56 -0800

    Send GA tracking and client ID to registration form.
    update app versioning
    
    rename var
    
    add tracking id along with client id

--- programs-remote-listings/resources/frontend/rs.js
+++ programs-remote-listings/resources/frontend/rs.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-// toggle program info link
+	// toggle program info link
     $('.rs-program-message-link').click(function (e) {
         e.preventDefault();
 
@@ -9,5 +9,33 @@ jQuery(document).ready(function($) {
             $('.rs-program-message-link span').text('+')
 
         $('.rs-program-message').slideToggle();
+    });
+
+    /* Google Analytics */
+    function rs_set_ga_tracker(tracker){
+    	window.rs_ga_client_id = tracker.get('clientId');
+        window.rs_ga_tracking_id = tracker.get('trackingId');
+    };
+    function rs_ga_loaded_cb(){
+    	if (rs_ga_retry_count >= 10){
+    		return;
+    	}
+    	if (typeof window.ga !== "undefined"){
+    		ga(rs_set_ga_tracker);
+    		return;
+    	}
+    	rs_ga_retry_count += 1;
+    	window.setTimeout(rs_ga_loaded_cb, 500);
+    };
+    var rs_ga_retry_count = 0;
+    window.setTimeout(rs_ga_loaded_cb, 100);
+
+    /* Bind event to Register Now to append the client ID if it exists. */
+    $(".rs-register-link a").click(function(e){
+    	if (window.rs_ga_client_id && window.rs_ga_tracking_id){
+    		e.preventDefault();
+    		location.href = $(this).prop("href") + "&ga-client-id=" + encodeURI(window.rs_ga_tracking_id) 
+                             + encodeURI('~') + encodeURI(window.rs_ga_client_id);
+    	}
     });
 });
--- programs-remote-listings/rs-connect.php
+++ programs-remote-listings/rs-connect.php
@@ -353,7 +353,7 @@ class RS_Connect
     function rs_enqueue_items()
     {
         wp_enqueue_style('rs-f', plugins_url('/resources/frontend/rs.css', __FILE__), null, '20151013a');
-        wp_enqueue_script('rs-js', plugins_url('/resources/frontend/rs.js', __FILE__), array('jquery'), '20150612a');
+        wp_enqueue_script('rs-js', plugins_url('/resources/frontend/rs.js', __FILE__), array('jquery'), '20160218');
 
         $options = get_option('rs_settings');
         $inline_styles = '';
