94e91fddd0ca21d3c41b570b0dfafe5f78fa91a1
Ryan Dury <ryandury@gmail.com>
2016-03-30 11:54:59 -0700

    Add teacher image back

--- programs-remote-listings/templates/shortcode-programs-single.php
+++ programs-remote-listings/templates/shortcode-programs-single.php
@@ -94,9 +94,13 @@ if (is_array($shortcode_atts)) extract($shortcode_atts); ?>
         <?php foreach($rs_the_program->teacher_details->teacher_objects as $teacher) : ?>
         <?php $teacher_url = $RS_Connect->get_page_url('teachers') . $teacher->ID.'/'.$teacher->slug; ?>
         <div class="teacher" style="clear:left; position:relative;">
+            <?php if (isset($teacher->photo_details->thumbnail)) {  ?>
             <div style="float:left; width:<?php echo $teacher->photo_details->thumbnail->width; ?>px; margin-right:20px;">
-                <a href="<?php echo $teacher_url; ?>" style="float:left; margin:5px 20px 10px 0;"></a>
+                <a href="<?php echo $teacher_url; ?>" style="float:left; margin:5px 20px 10px 0;">
+                    <img src="<?php echo $teacher->photo_details->thumbnail->url; ?>" style="float:left; margin:5px 20px 10px 0;">
+                </a>
             </div>
+            <?php } ?>
             <div style="overflow:hidden;">
                 <a href="<?php echo $teacher_url; ?>"><strong><?php echo $teacher->post_title; ?></strong></a><br/>
                 <?php echo $RS_Connect->excerpt($teacher->post_content); ?>
