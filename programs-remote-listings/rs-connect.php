1c4c7c399dab173d5eb4f6b29b59cc01097ee973
Deryk Wenaus <deryk@retreat.guru>
2017-01-18 11:30:20 -0800

    make version 5.2 compatible.

--- programs-remote-listings/rs-connect.php
+++ programs-remote-listings/rs-connect.php
@@ -162,7 +162,7 @@ class RS_Connect
             $program_id = get_query_var('rs_program');
             $this->program = RS_Connect_Api::get_program($program_id);
             $program_url = $this->get_page_url('programs').$this->program->ID.'/'.$this->program->slug;
-            $meta_description = $this->program->seo_description ?: wp_trim_words($this->program->text, 50, '...');
+            $meta_description = $this->program->seo_description ? $this->program->seo_description : wp_trim_words($this->program->text, 50, '...');
 
             if (! empty($this->program->text)) {
                 echo '<meta property="og:url" content="'.$program_url.'/" />'."\n";
