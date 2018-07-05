8cded044c26f96aa518e58f18843d7065e90ec56
Cloud Edwards <>
2018-07-04 23:07:09 -0700

    improve code style

--- programs-remote-listings/resources/admin/rs-admin.js
+++ programs-remote-listings/resources/admin/rs-admin.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($) {
 
-    $('#rs-plugin-settings-show-advanced').on('click', function(e){
+    $("#rs-plugin-settings-show-advanced").on("click", function(e){
         e.preventDefault();
 
-        $('.rs-plugin-settings-advanced-container').toggle();
-    })
+        $(".rs-plugin-settings-advanced-container").toggle();
+    });
 });
