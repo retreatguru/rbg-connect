e77d635c5f7d94a908c7ed4129c701e14626cab3
Andy Chute <andy@retreat.guru>
2023-08-16 11:26:15 -0700

    [DEV-6083] Fix additional PHP warnings (#50)
    * [DEV-6083] Fix additional PHP warnings
    
    * more safeguarding
    
    * Added more verbosity to the deploy script

--- programs-remote-listings/deploy.sh
+++ programs-remote-listings/deploy.sh
@@ -13,13 +13,13 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
- 
- 
+
 # Let's begin...
 echo ".........................................."
 echo 
 echo "Preparing to deploy wordpress plugin"
-echo 
+echo "MAKE SURE YOU ARE RUNNING THIS FROM WITHIN THE DOCKER CONTAINER AND ARE IN THE CORRECT FOLDER"
+echo
 echo ".........................................."
 echo 
 
@@ -46,7 +46,6 @@ svn co $SVNURL $SVNPATH
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 echo $SVNPATH
 
- 
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
 README.md
@@ -56,11 +55,14 @@ README.md
 echo "Changing directory to SVN and committing to trunk"
 cp -R $GITPATH/. $SVNPATH/trunk/
 cd $SVNPATH/trunk/
+
 # Add all new files that are not set to be ignored
+echo "Adding any newly created files not set to be ignored. It is safe to disregard any 'svn add' parameter errors if no new files were created"
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+
+echo "Committing changes to SVN repository"
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
- 
 echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
  
